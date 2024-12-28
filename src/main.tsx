c431e1031c73c6d09264e50672c900b6e206380a
hfritz34 <henryfritz00@gmail.com>
2024-12-28 12:52:35 -0500

    provider changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import './index.css'
 import App from './components/navigation/App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-  domain="dev-tfgyd3i2jqk0igxv.us.auth0.com"
-  clientId="NZ5N1xnHOdgdVuXNPoBuNydMhg83Oe0p"
-  authorizationParams = {{
+    domain="dev-tfgyd3i2jqk0igxv.us.auth0.com"
+    clientId="NZ5N1xnHOdgdVuXNPoBuNydMhg83Oe0p"
+    authorizationParams={{
       redirect_uri: `${import.meta.env.VITE_WEB_ADDRESS}/authorization`,
-  }}
+      audience: "https://prestige-auth0-resource",
+      scope: "openid profile email user-read-email user-read-private user-read-recently-played user-top-read user-read-currently-playing app-remote-control"
+    }}
   >
     <React.StrictMode>
       <App />
