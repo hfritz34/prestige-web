8b3ab1a34e512d46d682b6aa2c083b688dbac0a5
hfritz34 <henryfritz00@gmail.com>
2024-12-27 22:37:40 -0500

    new auth0 tenant

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-  domain="dev-u10jtlqih3lq02fh.us.auth0.com"
-  clientId="O1NAzxf4JzsPtFHbbuHf6yg05lDqgCrY"
+  domain="dev-tfgyd3i2jqk0igxv.us.auth0.com"
+  clientId="NZ5N1xnHOdgdVuXNPoBuNydMhg83Oe0p"
   authorizationParams = {{
       redirect_uri: `${import.meta.env.VITE_WEB_ADDRESS}/authorization`,
   }}
